22c64800b1839851ee86fead91a1d8b23cb098a1
Chris Fields II <chris.fields@thinaer.io>
2020-06-20 03:59:44 -0500

    chore: removed demo folder

--- demo/demo.js
+++
@@ -1,7 +0,0 @@
-import ProgressBar from '@chrisfieldsii/progressbar-vanilla';
-
-const root = document.getElementById('root');
-
-const ProgressBar1 = new ProgressBar().render();
-
-root.append(ProgressBar1);
