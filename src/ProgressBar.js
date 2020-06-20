12e470456adcc34483a63197a60e49682821eb6e
Chris Fields II <chrisjr06@gmail.com>
2020-06-20 07:11:25 -0500

    feat: + ability to pass promises for progress

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,24 +1,83 @@
 import './ProgressBar.css';
 
+// https://stackoverflow.com/questions/30564053/how-can-i-synchronously-determine-a-javascript-promises-state
+async function getPromiseState(promise) {
+  const test = {};
+  return Promise.race([promise, test]).then(
+    (v) => (v === test ? 'pending' : 'fulfilled'),
+    () => 'rejected'
+  );
+}
+
 /**
  * @desc Progress Bar.
  * Can take array of promises to control progress (uncontrolled) or
  * manually set progress (controlled).
  */
 class ProgressBar {
-  constructor({ promises = [] } = {}) {
-    this.promises = promises;
-
-    this.percentage = 0;
+  constructor() {
+    this.progress = 0;
     this.inProgress = false;
     this.Container = null;
     this.Bar = null;
-
-    this.render = this.render.bind(this);
-    this.setProgress = this.setProgress.bind(this);
+    this.interval = null;
   }
 
-  render() {
+  startProgress = () => {
+    this.progress = 0;
+    this.inProgress = true;
+  };
+
+  endProgress = () => {
+    this.inProgress = false;
+  };
+
+  /**
+   * @desc Set progress of Bar
+   * @param {number} progress Percentage 0-100
+   */
+  setProgress = (progress) => {
+    this.progress = progress;
+    this.Bar.style.width = progress + '%';
+    return this;
+  };
+
+  /**
+   * @desc Set progress based off of promises
+   * @param {Array.<Promise>} promises Array of promises
+   */
+  setProgressPromises(promises) {
+    if (!promises.length) return;
+
+    clearInterval(this.interval);
+    this.startProgress();
+
+    const numPromises = promises.length;
+    const step = 100 / numPromises;
+    let promisesAdded = 0;
+
+    this.interval = setInterval(() => {
+      promises.forEach(async (promise) => {
+        const promiseState = await getPromiseState(promise);
+        console.log(promiseState);
+
+        const isInDoneState = promiseState === 'fulfilled' || promiseState === 'rejected';
+        if (isInDoneState && !promise.addedToProgress) {
+          promise.addedToProgress = true;
+          const newProgress = promisesAdded * step + step;
+          this.setProgress(newProgress);
+          promisesAdded++;
+
+          // check if this was last promise to be added
+          if (promisesAdded === numPromises) {
+            this.endProgress();
+          }
+        }
+      });
+    }, 50);
+  }
+
+  render = () => {
     const Container = document.createElement('div');
     this.Container = Container;
     Container.classList.add('cf-progressbar-container');
@@ -32,17 +91,7 @@ class ProgressBar {
     this.setProgress(0);
 
     return Container;
-  }
-
-  /**
-   * @desc Set progress of Bar
-   * @param {number} percentage Percentage 0-100
-   */
-  setProgress(percentage) {
-    this.percentage = percentage;
-    this.Bar.style.width = percentage + '%';
-    return this;
-  }
+  };
 }
 
 export default ProgressBar;
--- src/ProgressBar.stories.js
+++ src/ProgressBar.stories.js
@@ -6,6 +6,38 @@ export default {
   title: 'Progress Bar Demo',
 };
 
+async function wait(time) {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res('done');
+    }, time * 1000);
+  });
+}
+const getPromises = () => new Array(9).fill('').map((o) => wait(Math.floor(Math.random() * 5)));
+
 export const ProgressBarDefault = () => {
   return new ProgressBar().render();
+};
+
+export const ProgresBarInterval = () => {
+  const Bar = new ProgressBar();
+  const Comp = Bar.render();
+  let progress = 0;
+
+  setInterval(() => {
+    progress += 20;
+    if (progress > 100) progress = 0;
+    Bar.setProgress(progress);
+  }, 1000);
+
+  return Comp;
+};
+
+export const ProgressBarPromises = () => {
+  const Bar = new ProgressBar();
+  const Comp = Bar.render();
+
+  Bar.setProgressPromises(getPromises());
+
+  return Comp;
 };
