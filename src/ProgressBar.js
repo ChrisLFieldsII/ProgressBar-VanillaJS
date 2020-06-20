1dea784899615e8185c62b5673c589bc5816eaba
Chris Fields II <chrisjr06@gmail.com>
2020-06-20 04:58:35 -0500

    + ability to SetProgress

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -3,30 +3,45 @@ import './ProgressBar.css';
 /**
  * @desc Progress Bar.
  * Can take array of promises to control progress (uncontrolled) or
- * take a progress prop to manually control (controlled).
+ * manually set progress (controlled).
  */
 class ProgressBar {
   constructor({ promises = [] } = {}) {
     this.promises = promises;
 
-    this.progress = 0;
+    this.percentage = 0;
     this.inProgress = false;
-    this.Component = null;
+    this.Container = null;
+    this.Bar = null;
 
     this.render = this.render.bind(this);
+    this.setProgress = this.setProgress.bind(this);
   }
 
   render() {
-    const Component = document.createElement('div');
-    this.Component = Component;
-    Component.classList.add('cf-progressbar-container');
+    const Container = document.createElement('div');
+    this.Container = Container;
+    Container.classList.add('cf-progressbar-container');
 
     const Bar = document.createElement('div');
+    this.Bar = Bar;
     Bar.classList.add('cf-progressbar');
 
-    Component.append(Bar);
+    Container.append(Bar);
 
-    return Component;
+    this.setProgress(0);
+
+    return Container;
+  }
+
+  /**
+   * @desc Set progress of Bar
+   * @param {number} percentage Percentage 0-100
+   */
+  setProgress(percentage) {
+    this.percentage = percentage;
+    this.Bar.style.width = percentage + '%';
+    return this;
   }
 }
 
