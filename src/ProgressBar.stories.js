47686fc8345252a318c2052d2dfc02d78da51417
Chris Fields II <chrisjr06@gmail.com>
2020-06-20 20:14:24 -0500

    fix: stories name convention, AutoHideOnEnd interval, + FormRow component

--- src/ProgressBar.stories.js
+++ src/ProgressBar.stories.js
@@ -13,20 +13,8 @@ function startProgressInterval({ Bar, progress = 0, repeat = true }) {
     else if (progress > 100 && !repeat) clearInterval(interval);
     Bar.setProgress(progress);
   }, 1000);
+  return interval;
 }
-
-export const ProgressBarDefault = () => {
-  return new ProgressBar().render();
-};
-
-export const ProgresBarInterval = () => {
-  const Bar = new ProgressBar();
-  const Comp = Bar.render();
-
-  startProgressInterval({ Bar });
-
-  return Comp;
-};
 
 async function wait(time) {
   return new Promise((res) => {
@@ -38,8 +26,37 @@ async function wait(time) {
 function getPromises(numPromises = 9, waitTime = 5) {
   return new Array(Number(numPromises)).fill('').map((_) => wait(Math.random() * Number(waitTime)));
 }
+function FormRow({ label = '', value = '', onChange }) {
+  const Container = document.createElement('div');
+  const Label = document.createElement('label');
+  const Input = document.createElement('input');
+  Label.innerHTML = label;
+  Input.type = 'number';
+  Input.value = value;
+  Input.style.margin = '10px';
+  Input.addEventListener('change', onChange);
+  Container.append(Label, Input);
+  return Container;
+}
 
-export const ProgressBarPromises = () => {
+export const Default = () => {
+  return new ProgressBar().render();
+};
+
+export const Interval = () => {
+  const Bar = new ProgressBar();
+  const Comp = Bar.render();
+
+  const Container = document.createElement('div');
+
+  startProgressInterval({ Bar });
+
+  Container.append(Comp);
+
+  return Container;
+};
+
+export const Promises = () => {
   const Bar = new ProgressBar();
   const Comp = Bar.render();
 
@@ -49,45 +66,36 @@ export const ProgressBarPromises = () => {
   const Container = document.createElement('div');
 
   // Create input to control number of promises
-  const NumPromisesContainer = document.createElement('div');
-  const NumPromisesLabel = document.createElement('label');
-  const NumPromisesInput = document.createElement('input');
-  NumPromisesLabel.innerHTML = 'How many Promises to load:&nbsp;';
-  NumPromisesInput.type = 'number';
-  NumPromisesInput.value = numPromises;
-  NumPromisesInput.style.margin = '10px';
-  NumPromisesInput.addEventListener('change', (e) => {
-    console.log(e);
-    numPromises = e.target.value;
-    Bar.setProgressPromises(getPromises(numPromises, maxWait));
+  const NumPromisesFormRow = FormRow({
+    label: 'How many Promises to load:&nbsp;',
+    value: numPromises,
+    onChange: (e) => {
+      console.log(e);
+      numPromises = e.target.value;
+      Bar.setProgressPromises(getPromises(numPromises, maxWait));
+    },
   });
-  NumPromisesContainer.append(NumPromisesLabel, NumPromisesInput);
+  NumPromisesFormRow.style.marginTop = '20px';
 
   // Create input to control max wait time of any promise
-  const MaxWaitTimeContainer = document.createElement('div');
-  const MaxWaitTimeLabel = document.createElement('label');
-  const MaxWaitTimeInput = document.createElement('input');
-  MaxWaitTimeLabel.innerHTML = 'Max time a Promise can wait:&nbsp;';
-  MaxWaitTimeInput.type = 'number';
-  MaxWaitTimeInput.value = maxWait;
-  MaxWaitTimeInput.style.margin = '10px';
-  MaxWaitTimeInput.addEventListener('change', (e) => {
-    console.log(e);
-    maxWait = e.target.value;
-    Bar.setProgressPromises(getPromises(numPromises, maxWait));
+  const MaxWaitTimeFormRow = FormRow({
+    label: 'Max time a Promise can wait:&nbsp;',
+    value: maxWait,
+    onChange: (e) => {
+      console.log(e);
+      maxWait = e.target.value;
+      Bar.setProgressPromises(getPromises(numPromises, maxWait));
+    },
   });
-  MaxWaitTimeContainer.append(MaxWaitTimeLabel, MaxWaitTimeInput);
 
   Bar.setProgressPromises(getPromises());
 
-  Comp.style.marginTop = '20px';
-
-  Container.append(NumPromisesContainer, MaxWaitTimeContainer, Comp);
+  Container.append(Comp, NumPromisesFormRow, MaxWaitTimeFormRow);
 
   return Container;
 };
 
-export const ProgressBarCustomStyle = () => {
+export const CustomStyle = () => {
   const Bar = new ProgressBar({
     innerBarStyle: {
       backgroundColor: 'orange',
@@ -105,7 +113,7 @@ export const ProgressBarCustomStyle = () => {
   return Comp;
 };
 
-export const ProgressBarInitialProgress = () => {
+export const InitialProgress = () => {
   const Bar = new ProgressBar({
     initProgress: 60,
   });
@@ -113,13 +121,14 @@ export const ProgressBarInitialProgress = () => {
   return Comp;
 };
 
-export const ProgressBarAutoHideOnEnd = () => {
+export const AutoHideOnEnd = () => {
+  let interval;
   const Bar = new ProgressBar({
     autoHideOnEnd: true,
   });
   const Comp = Bar.render();
 
-  startProgressInterval({ Bar, repeat: false });
+  interval = startProgressInterval({ Bar, repeat: false });
 
   const Container = document.createElement('div');
 
@@ -127,12 +136,15 @@ export const ProgressBarAutoHideOnEnd = () => {
   Btn.innerText = 'Reset';
   Btn.classList.add('btn', 'btn-primary', 'mt-3');
   Btn.addEventListener('click', () => {
+    clearInterval(interval);
     Bar.startProgress();
 
-    startProgressInterval({ Bar, repeat: false });
+    interval = startProgressInterval({ Bar, repeat: false });
   });
 
   Container.append(Comp, Btn);
 
   return Container;
 };
+
+// export const Pr
